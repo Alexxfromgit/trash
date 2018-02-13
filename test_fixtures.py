e4f719620ec00fa93dfcc3b1042641fbb0ad8130
Alexx <32265866+Alexxfromgit@users.noreply.github.com>
2018-02-13 20:11:34 +0200

    Update test_fixtures.py

--- test_fixtures.py
+++ test_fixtures.py
@@ -4,12 +4,21 @@ import pytest
 @pytest.fixture()
 def before():
     print('\nbefore each test')
-    
-    
-def test_1(before):
-    print('test_1()')
-    
-    
-def test_2(before):
-    print('test_2()')
 
+
+def test_1(before):
+    print('\ntest_1()')
+
+
+def test_2(before):
+    print('\ntest_2()')
+
+
+@pytest.mark.usefixtures("before")
+def test_3():
+    print('test_3()')
+
+
+@pytest.mark.usefixtures("before")
+def test_4():
+    print('test_4()')
