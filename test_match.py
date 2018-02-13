839d61a4c84c4f1ac7f50cb3fca69cf1c56993c3
Alexx <32265866+Alexxfromgit@users.noreply.github.com>
2018-02-13 19:17:48 +0200

    Create test_match.py

---
+++ test_match.py
@@ -0,0 +1,14 @@
+from unnecessary_math import multiply
+
+
+def test_numbers_3_4():
+    print('test_numbers_3_4 <======================= actual test code')
+    assert multiply(3, 4) == 12
+
+
+def test_strings_a_3():
+    print('test_strings_a_2 <======================= actual test code')
+    assert multiply('a', 3) == 'aaa'
+
+
+
