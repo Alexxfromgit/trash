2af41ee1551349562a25801416b79574799e3977
Alexx <32265866+Alexxfromgit@users.noreply.github.com>
2018-04-01 00:51:54 +0300

    Update tasktest2.sh

--- tasktest2.sh
+++ tasktest2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #
+# >/dev/null redirects the command standard output to the null device, 
+# which is a special device which discards the information written to it. 
+# 2 >&1 redirects the standard error stream
+# to the standard output stream (stderr = 2, stdout = 1).
+#
+exec 1> /dev/null 2>&1
